6ad412364f81e285bd8170138641d316d78611da
Janez K <janez.kranjc@gmail.com>
2014-10-12 00:09:16 +0200

    nicer admin

--- tweetset/collect/admin.py
+++ tweetset/collect/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
 
 from collect.models import Collection, Tweet
+from django.db.models import Count
 
-admin.site.register(Collection)
-admin.site.register(Tweet)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('name','is_running','exists','user','tweetcount')
+
+    def queryset(self,request):
+        return super(CollectionAdmin,self).queryset(request).annotate(tweetcount=Count('tweets'))
+
+    def is_running(self,obj):
+        return obj.is_running()
+
+    def exists(self,obj):
+        return obj.exists()
+
+    def tweetcount(self,obj):
+        return obj.tweetcount
+
+    is_running.boolean = True
+    exists.boolean = True
+
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ('twitter_id','text','collection')
+
+    def text(self,obj):
+        return obj.data['text']
+
+admin.site.register(Collection,CollectionAdmin)
+admin.site.register(Tweet,TweetAdmin)
