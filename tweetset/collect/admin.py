c6acfb9f12a05ced5bea3038e103854e6bcaf60e
Janez K <janez.kranjc@gmail.com>
2014-10-13 10:51:45 +0200

    fixed up admin and changed twitter api credentials from text to charfield

--- tweetset/collect/admin.py
+++ tweetset/collect/admin.py
@@ -3,11 +3,27 @@ from django.contrib import admin
 from collect.models import Collection, Tweet
 from django.db.models import Count
 
+def start_collections(modeladmin, request, queryset):
+    for c in queryset:
+        c.start()
+start_collections.short_description = "Start selected collections"
+
+def stop_collections(modeladmin, request, queryset):
+    for c in queryset:
+        c.stop()
+stop_collections.short_description = "Stop selected collections"
+
+def empty_collections(modeladmin, request, queryset):
+    for c in queryset:
+        c.tweets.all().delete()
+empty_collections.short_description = "Remove all tweets from selected collections"
+
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ('name','is_running','exists','user','tweetcount')
+    raw_id_fields = ('user',)
 
-    def queryset(self,request):
-        return super(CollectionAdmin,self).queryset(request).annotate(tweetcount=Count('tweets'))
+    def get_queryset(self,request):
+        return super(CollectionAdmin,self).get_queryset(request).annotate(tweetcount=Count('tweets'))
 
     def is_running(self,obj):
         return obj.is_running()
@@ -20,9 +36,11 @@ class CollectionAdmin(admin.ModelAdmin):
 
     is_running.boolean = True
     exists.boolean = True
+    actions = [start_collections,stop_collections,empty_collections]
 
 class TweetAdmin(admin.ModelAdmin):
     list_display = ('twitter_id','text','collection')
+    raw_id_fields = ('collection',)
 
     def text(self,obj):
         return obj.data['text']
--- tweetset/collect/models.py
+++ tweetset/collect/models.py
@@ -12,22 +12,26 @@ class Collection(models.Model):
     track = models.TextField(blank=True,null=True,help_text="A comma separated list of keywords or phrases to track. Phrases of keywords are specified by a comma-separated list. More information at https://dev.twitter.com/docs/streaming-apis/parameters#track")
     locations = models.TextField(blank=True,null=True,help_text="A comma-separated list of longitude,latitude pairs specifying a set of bounding boxes to filter Tweets by. On geolocated Tweets falling within the requested bounding boxes will be included—unlike the Search API, the user\'s location field is not used to filter tweets. Each bounding box should be specified as a pair of longitude and latitude pairs, with the southwest corner of the bounding box coming first. For example: \"-122.75,36.8,-121.75,37.8\" will track all tweets from San Francisco. NOTE: Bounding boxes do not act as filters for other filter parameters. More information at https://dev.twitter.com/docs/streaming-apis/parameters#locations")
     firehose = models.BooleanField(default=False,help_text="Use this option to receive all public tweets if there are no keywords, users or locations to track. This requires special permission from Twitter. Otherwise a sample of 1 percent of tweets will be returned.")
-    consumer_key = models.TextField(max_length=150,help_text="The consumer key that you obtain when you create an app at https://apps.twitter.com/")
-    consumer_secret = models.TextField(max_length=150,help_text="The consumer secret that you obtain when you create an app at https://apps.twitter.com/")
-    access_token = models.TextField(max_length=150,help_text="You can generate your user access token at http://apps.twitter.com by clicking 'Create my access token'.")
-    access_token_secret = models.TextField(max_length=150,help_text="You can generate your user access token secret at http://apps.twitter.com by clicking 'Create my access token'.")
+    consumer_key = models.CharField(max_length=150,help_text="The consumer key that you obtain when you create an app at https://apps.twitter.com/")
+    consumer_secret = models.CharField(max_length=150,help_text="The consumer secret that you obtain when you create an app at https://apps.twitter.com/")
+    access_token = models.CharField(max_length=150,help_text="You can generate your user access token at http://apps.twitter.com by clicking 'Create my access token'.")
+    access_token_secret = models.CharField(max_length=150,help_text="You can generate your user access token secret at http://apps.twitter.com by clicking 'Create my access token'.")
     user = models.ForeignKey(User,related_name="collections")
 
     def start(self):
         s = xmlrpclib.ServerProxy(settings.SUPERVISOR_URI)
         if not self.exists():
-            s.twiddler.addProgramToGroup('tweetset', 'collection'+str(self.pk), 
+            try:
+                s.twiddler.addProgramToGroup('tweetset', 'collection'+str(self.pk), 
                 {'command':settings.PYTHON_EXECUTABLE+' '+os.path.join(settings.PROJECT_DIR,'manage.py')+' stream '+str(self.pk),
                 'autostart':'false', 
                 'autorestart':'true', 
                 'startsecs':'3'})
+            except:
+                return False
         if not self.is_running():
             s.supervisor.startProcess('tweetset:collection'+str(self.pk))
+            return True
 
     def stop(self):
         s = xmlrpclib.ServerProxy(settings.SUPERVISOR_URI)
@@ -38,7 +42,10 @@ class Collection(models.Model):
 
     def exists(self):
         s = xmlrpclib.ServerProxy(settings.SUPERVISOR_URI)
-        l = s.supervisor.getAllProcessInfo()
+        try:
+            l = s.supervisor.getAllProcessInfo()
+        except:
+            return False
         names = [x['name'] for x in l]
         if 'collection'+str(self.pk) in names:
             return True
